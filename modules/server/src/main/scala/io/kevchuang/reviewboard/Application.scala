015ddd25669523d0239c7252565d39086f2aea99
Kevin Chuang <kevin.chuang@contentsquare.com>
2024-02-22 23:17:19 +0100

    feat(server): Implement HttpEndpoint interface

--- modules/server/src/main/scala/io/kevchuang/reviewboard/Application.scala
+++ modules/server/src/main/scala/io/kevchuang/reviewboard/Application.scala
@@ -1,6 +1,6 @@
 package io.kevchuang.reviewboard
 
-import io.kevchuang.reviewboard.http.controllers.HealthController
+import io.kevchuang.reviewboard.http.endpoints.HealthEndpoint
 import io.kevchuang.reviewboard.http.server.{HttpServer, HttpServerLive}
 import zio.*
 import zio.http.Server
@@ -9,9 +9,9 @@ object Application extends ZIOAppDefault:
 
   private val program: ZIO[HttpServer & Server, Throwable, Unit] =
     for
-      health   <- HealthController.make
-      endpoints = List(health.healthCheck)
-      _        <- HttpServer.start(endpoints)
+      healthEndpoint <- HealthEndpoint.make
+      endpoints       = List(healthEndpoint.serverEndpoint)
+      _              <- HttpServer.start(endpoints)
     yield ()
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
