15f36eb3db57a6a367f229e95e5aaf473e63a8df
Kevin Chuang <kevin.chuang@contentsquare.com>
2024-02-23 01:55:29 +0100

    feat(server): Implement HttpRoute interface

--- modules/server/src/main/scala/io/kevchuang/reviewboard/http/server/HttpServer.scala
+++ modules/server/src/main/scala/io/kevchuang/reviewboard/http/server/HttpServer.scala
@@ -6,13 +6,13 @@ import zio.http.Server
 
 trait HttpServer:
   def start(
-      endpoints: List[ServerEndpoint[Any, Task]]
+      routes: List[ServerEndpoint[Any, Task]]
   ): ZIO[Server, Throwable, Unit]
 end HttpServer
 
 object HttpServer:
   def start(
-      endpoints: List[ServerEndpoint[Any, Task]]
+      routes: List[ServerEndpoint[Any, Task]]
   ): ZIO[HttpServer & Server, Throwable, Unit] =
-    ZIO.serviceWithZIO[HttpServer](_.start(endpoints))
+    ZIO.serviceWithZIO[HttpServer](_.start(routes))
 end HttpServer
